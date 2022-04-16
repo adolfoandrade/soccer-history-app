65ded3111273912852f27d2169a4e15d1e1db218
adolfo.andrade <adolfo.j.de.andrade@avanade.com>
2022-04-16 00:40:43 -0300

    Configs

--- backend/App.Admin.Api/App.Admin.Api/Startup.cs
+++ backend/App.Admin.Api/App.Admin.Api/Startup.cs
@@ -63,9 +63,10 @@ namespace App.Admin.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "App.Admin.Api v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "App.Admin.Api v1"));
 
             app.UseHttpsRedirection();
 
